2a286f3d9186ccc7234b07c4990f03346468a31a
Arif <arifhida1647@gmail.com>
2025-01-19 17:13:15 +0700

    update 1.0

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'sneaklab-frontend-production.up.railway.app';
+    public string $baseURL = 'https://sneaklab-frontend-production.up.railway.app/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
